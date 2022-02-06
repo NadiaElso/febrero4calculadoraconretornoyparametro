071c085fe5b465e27eae8fc03ebb3b089cdb0008
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2022-02-06 14:44:58 -0300

    febrero6calculadora2conparámtroyretorno

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,78 @@
-function suma(operacion: number, numero1: number, numero2: number): number {
-  if (operacion === 1) {
-    let suma: Number = numero1 + numero2;
+let operaciondecalculo: number = Number(
+  prompt(
+    "Ingrese el tipo de calculo que desea hacer:1 para suma, 2 para resta, 3 para multiplicación y 4 para división"
+  )
+);
 
-    console.log(numero1);
-    console.log("+");
-    console.log(numero2);
-    console.log("El resultado de la suma es " + suma);
-  } else {
-    console.log("para realizar una suma debe ingresar el 1 como operador");
-  }
+function suma(numero1: number, numero2: number): number {
+  let suma: Number = numero1 + numero2;
+
+  console.log(numero1);
+  console.log("+");
+  console.log(numero2);
+  console.log("El resultado de la suma es " + suma);
+
   return;
 }
 
-function resta(operacion: number, numero1: number, numero2: number): number {
-  if (operacion === 2) {
-    let resta: number = numero1 - numero2;
+function resta(numero1: number, numero2: number): number {
+  let resta: number = numero1 - numero2;
 
-    console.log(numero1);
-    console.log("-");
-    console.log(numero2);
-    console.log("El resultado de la resta es " + resta);
-  } else {
-    console.log("para realizar una resta debe ingresar el 2 como operador");
-  }
+  console.log(numero1);
+  console.log("-");
+  console.log(numero2);
+  console.log("El resultado de la resta es " + resta);
+
   return;
 }
 
-function multiplicar(
-  operacion: number,
-  numero1: number,
-  numero2: number
-): number {
-  if (operacion === 3) {
-    let multiplicar: number = numero1 * numero2;
+function multiplicar(numero1: number, numero2: number): number {
+  let multiplicar: number = numero1 * numero2;
 
-    console.log(numero1);
-    console.log("*");
-    console.log(numero2);
-    console.log("El resultado de la multiplicación es " + multiplicar);
-  } else {
-    console.log(
-      "para realizar una multiplicaciòn debe ingresar el 3 como operador"
-    );
-  }
+  console.log(numero1);
+  console.log("*");
+  console.log(numero2);
+  console.log("El resultado de la multiplicación es " + multiplicar);
+
   return;
 }
 
-function dividir(operacion: number, numero1: number, numero2: number): number {
-  if (operacion === 4) {
-    let dividir = numero1 / numero2;
+function dividir(numero1: number, numero2: number): number {
+  let dividir = numero1 / numero2;
 
-    console.log(numero1);
-    console.log("/");
-    console.log(numero2);
-    console.log("El resultado de la división es " + dividir);
-  } else {
-    console.log("para realizar una divisiòn debe ingresar el 4 como operador");
-  }
+  console.log(numero1);
+  console.log("/");
+  console.log(numero2);
+  console.log("El resultado de la división es " + dividir);
+
   return;
 }
 
-let totalsuma: number = suma(1, 3, 4);
+switch (operaciondecalculo) {
+  case 1:
+    let totalsuma: number = 0;
+    totalsuma = suma(1, 3);
 
-console.log(totalsuma);
+    console.log(totalsuma);
+    break;
+  case 2:
+    let totalresta: number = 0;
+    totalresta = resta(2, 6);
 
-let totalresta: number = resta(2, 6, 4);
+    console.log(totalresta);
+    break;
+  case 3:
+    let totalmultiplicacion: number = 0;
+    totalmultiplicacion = multiplicar(5, 6);
 
-console.log(totalresta);
+    console.log(totalmultiplicacion);
+    break;
+  case 4:
+    let totaldivision: number = 0;
+    totaldivision = dividir(4, 2);
 
-let totalmultiplicacion: number = multiplicar(3, 2, 6);
-
-console.log(totalmultiplicacion);
-
-let totaldivision: number = dividir(4, 2, 3);
-
-console.log(totaldivision);
+    console.log(totaldivision);
+    break;
+  default:
+    break;
+}
